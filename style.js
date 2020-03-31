b13e05526227a9e54be193b3b4d51928bf8f23a2
Ana Berg <ana.berg99@gmail.com>
2020-03-31 15:24:20 -0300

    Create style.js

---
+++ style.js
@@ -0,0 +1,13 @@
+function eatSound(){
+    let src = 'sounds/eatSound.mp3';
+    let audio = document.getElementById("sound");
+    audio.src = src;
+    audio.play();
+}
+
+function defeatSound(){
+    let src = 'sounds/defeatSound.mp3';
+    let audio = document.getElementById("sound");
+    audio.src = src;
+    audio.play();
+}
